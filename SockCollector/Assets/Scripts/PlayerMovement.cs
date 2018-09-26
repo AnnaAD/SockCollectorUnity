f5148b5dd8b8cdc76380d15dcbe5997146431a6f
Anna AD <anna.arpacidusseau@gmail.com>
2018-09-26 12:40:27 -0500

    Added Player and testWall
    Made playerMovement script and a testWall. Using rigidbody2d for physics with boxcolliders.

---
+++ SockCollector/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rb;
+    //integer (number) value to multiply movement by
+    [SerializeField] private int speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //set player velocity based on horizontal and vertical input
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
+	}
+}
