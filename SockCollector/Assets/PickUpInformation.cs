83a9173182f58fca6e311a1fdb791c0cabfa4b88
Anna AD <anna.arpacidusseau@gmail.com>
2018-10-17 12:29:14 -0500

    added testWall sprite and pickup.

---
+++ SockCollector/Assets/PickUpInformation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpInformation : MonoBehaviour {
+    private string type;
+    private int value;
+
+    public string getType() {
+        return type;
+    }
+
+    public string getValue() {
+        return value;
+    }
+
+
+
+}
