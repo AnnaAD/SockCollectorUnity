db6f1c13b6a1e9dcb5182dd7918e7ef110bd2acf
Anna AD <anna.arpacidusseau@gmail.com>
2018-10-17 12:43:21 -0500

    started working on world manager and pickups

--- SockCollector/Assets/PickUpController.cs
+++ SockCollector/Assets/PickUpController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script will be on the player, allowing for the player to detect when it 
+//is picking up a pick-upable item.
 public class PickUpController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GameObject worldManager;
+
+    private void Start() {
+        worldManager = GameObject.Find("WorldManager");
+    }
+
+    //On every collision this method checks if the object colliding with
+    //has the "PickUp" tag. If so, it deletes it on collision.
+    public void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "PickUp") {
+            //TODO: add scripts to update player inventory, stats, score, etc.
+            PickUpInformation pickupInfo = collision.gameObject.GetComponent<PickUpInformation>();
+            if (pickupInfo.getType() == "sock") {
+                worldManager.GetComponent<PlayerData>().addSocks(pickupInfo.getValue());
+            }
+            Destroy(collision.gameObject);
+        }
+    }
 }
